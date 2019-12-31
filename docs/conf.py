e6ba5d47378edea80eb9dcb04f09f86c8d7ff736
Shawn Brown <shawnbrown@users.noreply.github.com>
2019-12-31 18:06:52 -0500

    Correct copyright date in conf.py file.

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'squint'
-copyright = '2014 - 2020, National Committee for an Effective Congress, et al.'
+copyright = '2015 - 2020, National Committee for an Effective Congress, et al.'
 author = 'Shawn Brown'
 
 # The version info for the project you're documenting, acts as replacement for
