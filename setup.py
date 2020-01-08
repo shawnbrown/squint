97af6219751cc331bca8f306c12cd094f55850ee
Shawn Brown <shawnbrown@users.noreply.github.com>
2020-01-07 22:43:46 -0500

    Add relative directory handling to setup.py installation.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import ast
+import os
 import setuptools
 
 
@@ -19,42 +20,49 @@ def get_version(path):
     raise Exception('Unable to find __version__ attribute.')
 
 
-setuptools.setup(
-    # Required fields:
-    name='squint',
-    version=get_version('squint/__init__.py'),
-    description='Simple query interface for tabular data.',
-    packages=setuptools.find_packages(exclude=['docs', 'tests']),
+if __name__ == '__main__':
+    original_dir = os.path.abspath(os.getcwd())
+    try:
+        os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
-    # Recommended fields:
-    url='https://github.com/shawnbrown/squint',
-    author='Shawn Brown',
-    author_email='shawnbrown@users.noreply.github.com',
+        setuptools.setup(
+            # Required fields:
+            name='squint',
+            version=get_version('squint/__init__.py'),
+            description='Simple query interface for tabular data.',
+            packages=setuptools.find_packages(exclude=['docs', 'tests']),
 
-    # Other fields:
-    install_requires=[
-        'get-reader[excel,dbf]',
-    ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
-    long_description=get_long_description('README.rst'),
-    long_description_content_type='text/x-rst',
-    license='Apache 2.0',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Utilities',
-    ],
-)
+            # Recommended fields:
+            url='https://github.com/shawnbrown/squint',
+            author='Shawn Brown',
+            author_email='shawnbrown@users.noreply.github.com',
+
+            # Other fields:
+            install_requires=[
+                'get-reader[excel,dbf]',
+            ],
+            python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+            long_description=get_long_description('README.rst'),
+            long_description_content_type='text/x-rst',
+            license='Apache 2.0',
+            classifiers=[
+                'Development Status :: 4 - Beta',
+                'Intended Audience :: Developers',
+                'License :: OSI Approved :: Apache Software License',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python :: 2',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5',
+                'Programming Language :: Python :: 3.6',
+                'Programming Language :: Python :: 3.7',
+                'Programming Language :: Python :: 3.8',
+                'Programming Language :: Python :: Implementation :: CPython',
+                'Programming Language :: Python :: Implementation :: PyPy',
+                'Topic :: Scientific/Engineering :: Information Analysis',
+                'Topic :: Utilities',
+            ],
+        )
+    finally:
+        os.chdir(original_dir)
